5f71345f830c3d20e050c460da914921ee7823ec
neil <nilnil47@gmail.com>
2021-03-31 21:44:19 +0300

    add environment to docker compose

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,16 @@
-FROM ubuntu
+#FROM ubuntu
+FROM java-base
+#
+#COPY . /app
+#WORKDIR /app
+#
+#RUN apt update && apt install -y maven
 
+RUN rm -rf /app
 COPY . /app
 WORKDIR /app
-
-RUN apt update && apt install -y maven && mvn package
+RUN mvn package
+ENV DB_NAME=default
+ENV DB_NAME=postgres
 ENV DB_NAME=default
 ENTRYPOINT ["java", "-jar", "/app/target/spring-boot-postgresql-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/hendisantika/postgres/DemoApplication.java
+++ src/main/java/com/hendisantika/postgres/DemoApplication.java
@@ -1,14 +1,26 @@
 package com.hendisantika.postgres;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+	@Value("${spring.datasource.url}")
+	private String userBucketPath;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("----------------------------------------------");
+		System.out.println(userBucketPath);
+		System.out.println("----------------------------------------------");
 	}
 }
